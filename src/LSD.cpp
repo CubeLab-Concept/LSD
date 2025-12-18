8d379616d1c0dd828680af19785cbc5aa117e88a
qbonenfa <quentin.bonenfant.ext@kiongroup.com>
2025-12-18 11:25:30 +0100

    Extend the lib to 16 chained modules.

--- src/LSD.cpp
+++ src/LSD.cpp
@@ -2,21 +2,25 @@
 #include "LSD.h"
 
 
-LSD::LSD(uint8_t data_pin, uint8_t clock_pin):
-         _data_pin(data_pin),
-         _clock_pin(clock_pin)
-{
-    /* LSD class constructor*/
+LSD::LSD(uint8_t data_pin, uint8_t clock_pin, uint8_t nb_module):
+_data_pin(data_pin), _clock_pin(clock_pin),
+{    /* LSD class constructor*/
 
+    
     // Define data and clock pins as outputs
-    pinMode(_data_pin, OUTPUT);
-    pinMode(_clock_pin, OUTPUT);
+    pinMode(this->_data_pin, OUTPUT);
+    pinMode(this->_clock_pin, OUTPUT);
+
 }
 
+LSD::LSD(uint8_t data_pin, uint8_t clock_pin)
+{
+    /* Constructeur pour un unique module P9813 */
+    LSD(data_pin, clock_pin, 1);
+}
 
-void LSD::set_color(uint8_t red, uint8_t green, uint8_t blue) {
-    /* Set the LED color to the RGB values in parameters (between 0-255) */
-    
+uint32_t LSD::format_color(uint8_t red, uint8_t green, uint8_t blue){
+    /* Format a color to PS9813 data format */
     uint32_t col_data = 0;
 
     // P9813 frame formatting
@@ -30,25 +34,108 @@ void LSD::set_color(uint8_t red, uint8_t green, uint8_t blue) {
     col_data |= (uint32_t)green << 8;      // 8 bit for each color
     col_data |= red;                       // MSB first, same order
 
-    // Send color data to IC
-    send_color(col_data);
+    return(col_data);
 }
 
 
-void LSD::set_color(uint32_t hex_color) {
-    /* Set color data from hex value */
+////////////////////////////////////////////////////////////////////////////
+// set_colors definition and overloads
+//
+// Single module setters
+//
+void LSD::set_color(uint8_t red, uint8_t green, uint8_t blue, uint8_t module_id, bool doShow){
+    /* Set the LED color to the RGB values in parameters (between 0-255)
+    The module ID can be set.
+    Data is only sent immediatly if doShow is true
+    */
+    
+    uint32_t col_data;
+
+    // Compute color
+    col_data = this->format_color(red, green, blue);
+    this->_led_colors[module_id] = col_data;    
+
+    // Send color data to IC?
+    if(doShow){
+        send_colors();
+    }
+}
+
+
+void LSD::set_color(uint8_t red, uint8_t green, uint8_t blue, uint8_t module_id){
+    /*Apply RGB color to module_id module and display it*/
+    set_color(red,green, blue, module_id, true);
+}
+
+
+void LSD::set_color(uint32_t hex_color, uint8_t module_id, bool doShow){
+    /* Set color data from hex value to specific moudule. Only show if required*/
     uint8_t red   = (hex_color >> 16) & 0xFF;
     uint8_t green = (hex_color >> 8)  & 0xFF;
     uint8_t blue  =  hex_color        & 0xFF;
-    set_color(red, green, blue);
+    set_color(red, green, blue, module_id, doShow);
 }
 
+void LSD::set_color(uint32_t hex_color, uint8_t module_id){
+    /* Set color data from hex value to specific moudule. Always apply immediatly*/
+    set_color(hex_color, module_id, true);
+}
 
-void LSD::send_color(uint32_t col_data) {
-    /* Send the color data to the P9813 IC */
+////////////////////////////////////////////////////////////////////////////
+// All module setters
+//
+void LSD::set_color(uint8_t red, uint8_t green, uint8_t blue, bool doShow){
+    /* Apply colors to all modules */
+    for(int i = 0; i< this->_nb_modules; i++){
+        set_color(red, green, blue, i, false);
+    }
+
+    // Send color data to IC?
+    if(doShow){
+        send_colors();
+    }
+}
+void LSD::set_color(uint8_t red, uint8_t green, uint8_t blue){
+    set_color(red, green, blue, true);
+}
+
+void LSD::set_color(uint32_t hex_color, bool doShow){
+    /*Apply hex colors to all modules, show if it is set*/
+    for(int i = 0; i< this->_nb_modules; i++){
+        set_color(hex_color i, false);
+    }
+
+    // Send color data to IC?
+    if(doShow){
+        send_colors();
+    }
+}
+
+void LSD::set_color(uint32_t hex_color) {
+    /*Apply hex colors to all modules and immediatly send data to IC*/
+    set_color(hex_color, true);
+}
+
+//
+//  End of set_colors
+///////////////////////////////////////////////////////////////////////////////
+
+void LSD::send_colors() {
+    /* Send the colors data to the P9813 IC */
     
     // Empty frame as separator
     empty_frame();
+
+    // Transmet l'ensemble des données LED aux P9813
+    for(int i =0; i< this->_nb_modules; i++){
+        send_frame(this->_led_colors[i]);
+    }
+    // Empty frame to end transmission
+    empty_frame();
+}
+
+void LSD::send_frame(uint32_t frame){
+    /* Transmer un bloc de 32bit de données au P9813*/
     uint32_t bit_index;
 
     // Sendata_ping color data bits, MSB first
@@ -58,15 +145,12 @@ void LSD::send_color(uint32_t col_data) {
         bit_index = 0x80000000 >> i; 
 
         // Digital write either HIGH or LOW dependata_ping on the masked bit
-        digitalWrite(this->_data_pin, ((col_data & bit_index) != 0) ? HIGH : LOW);
+        digitalWrite(this->_data_pin, ((frame & bit_index) != 0) ? HIGH : LOW);
 
         // Tick once
         tick();
     }
-    // Empty frame to end transmission
-    empty_frame();
 }
-
 
 void LSD::empty_frame() {
     /* Send an empty frame */
--- src/LSD.h
+++ src/LSD.h
@@ -8,19 +8,34 @@ class LSD {
     public:
 
         LSD(uint8_t data_pin, uint8_t clock_pin);
+        LSD(uint8_t data_pin, uint8_t clock_pin, uint8_t nb_module);
 
         // Color setter
+        void set_color(uint8_t red, uint8_t green, uint8_t blue, uint8_t module_id, bool doShow);
+        void set_color(uint8_t red, uint8_t green, uint8_t blue, uint8_t module_id);
+        void set_color(uint32_t hex, uint8_t module_id, bool doShow);
+        void set_color(uint32_t hex, uint8_t module_id);
+        
+        void set_color(uint8_t red, uint8_t green, uint8_t blue, bool doShow);
         void set_color(uint8_t red, uint8_t green, uint8_t blue);
+        void set_color(uint32_t hex, bool doShow);
         void set_color(uint32_t hex);
 
     private:
+        // Data and clock pins
         uint8_t _data_pin, _clock_pin;
-        uint16_t _clock_speed = 20; // 20 ms clock speed
+        uint8_t _nb_modules; // Set number of PC9813 (16 max)
+        unsigned _clock_speed = 1; // 1 ms = 1kHz clock
+        
+        // PC9813 formated color data, 16 modules max
+        uint32_t _led_colors[16]; 
 
         // private methods
         void tick();
         void set_clock_speed(uint16_t clock_speed);
-        void send_color(uint32_t col_data);
+        uint32_t format_color(uint32_t col_data);
+        void send_colors();
+        void send_frame(uint32_t frame);
         void empty_frame();
 };
 
